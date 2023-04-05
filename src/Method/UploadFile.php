a80eb312cc60100dbf38e63578d6cc040340fbc3
YixiongQiu <qiuyix@sina.cn>
2023-04-05 17:19:31 +0800

    upload file

---
+++ src/Method/UploadFile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tingyu\HttpRequest\Method;
+
+use tingyu\HttpRequest\HttpClient;
+
+class UploadFile extends HttpClient
+{
+    function do(string $uri, $data)
+    {
+        // 默认使用post船速
+
+
+
+
+
+    }
+
+
+}
